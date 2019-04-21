7d72aae32f6e836dc6d5e7b2c620322c5211ea28
Andy Palmer <atp@sdf.org>
2019-04-21 17:25:09 -0500

    Initialize repository

---
+++ Enumeration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Enumerations
+{
+    public sealed class Color
+    {
+        public static readonly Color Blue = new Color("blue");
+        public static readonly Color Red = new Color("red");
+        public static readonly Color Green = new Color("green");
+
+        private readonly string colorName;
+
+        private Color(string colorName)
+        {
+            this.colorName = colorName;
+        }
+
+        public override string ToString()
+        {
+            return this.colorName;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.colorName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return object.ReferenceEquals(this, obj);
+        }
+    }
+}
